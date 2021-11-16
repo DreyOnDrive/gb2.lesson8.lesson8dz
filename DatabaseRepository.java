6880b36818a1dc32931fc6f9e33c9c884ba230ef
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-11-16 17:20:25 +0300

    Update DatabaseRepository.java

--- DatabaseRepository.java
+++ DatabaseRepository.java
@@ -1,1 +1,12 @@
+package gb2.lesson8.lesson8dz;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DatabaseRepository {
+
+    boolean saveWeatherData(WeatherData weatherData) throws SQLException;
+
+    List<WeatherData> getAllSavedData() throws IOException, SQLException;
+}
