39855f6638daf7b85f030993aed26dce33228379
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-11-16 17:25:30 +0300

    Update WeatherProvider.java

--- WeatherProvider.java
+++ WeatherProvider.java
@@ -1,1 +1,12 @@
+package gb2.lesson8.lesson8dz;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface WeatherProvider {
+
+    void getWeather(Periods periods) throws IOException, SQLException;
+
+    List<WeatherData> getAllFromDb() throws IOException, SQLException;
+}
