2c974aefeeac87c06b000736c4dacef791d4eb1f
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-11-16 17:19:03 +0300

    Update ApplicationGlobalState.java

--- ApplicationGlobalState.java
+++ ApplicationGlobalState.java
@@ -1,1 +1,35 @@
+package gb2.lesson8.lesson8dz;
 
+public final class ApplicationGlobalState {
+
+    private static ApplicationGlobalState INSTANCE;
+    private String selectedCity = null;
+    private final String API_KEY = "urqfZr1ItLZCVsYFAn7izMmtoJCz4twG";
+    private final String DB_FILENAME = "application.db";
+
+    private ApplicationGlobalState() {
+    }
+
+    public static ApplicationGlobalState getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ApplicationGlobalState();
+        }
+        return INSTANCE;
+    }
+
+    public String getDbFileName() {
+        return DB_FILENAME;
+    }
+
+    public String getSelectedCity() {
+        return selectedCity;
+    }
+
+    public void setSelectedCity(String selectedCity) {
+        this.selectedCity = selectedCity;
+    }
+
+    public String getApiKey() {
+        return this.API_KEY;
+    }
+}
